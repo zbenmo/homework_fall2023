e8bea33efb91dd2b2b991fc2d7a7a3ec1e873590
Oren <zbenmo@gmail.com>
2024-07-19 12:42:39 +0200

    seems to be needed

--- hw1/setup.py
+++ hw1/setup.py
@@ -1,8 +1,8 @@
 # setup.py
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='cs285',
     version='0.1.0',
-    packages=['cs285'],
+    packages=['cs285'] + find_packages(),
 )
